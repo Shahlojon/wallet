6e73428f089a6a47f7b6e79dff81f27bf9b3d5c4
Shahlo <jaborova.shahlo@mail.ru>
2020-10-06 03:28:03 +0500

    func FavoritePayment

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -38,96 +38,6 @@ func TestService_FindAccoundById_Method_NotFound(t *testing.T) {
 	}
 }
 
-// func TestService_Reject_success(t *testing.T)  {
-// 	s := &Service{}
-
-// 	phone := types.Phone("+9920000001")
-// 	account, err := s.RegisterAccount(phone)
-// 	if err != nil {
-// 		t.Errorf("Reject(): can't register account, error = %v", err)
-// 		return
-// 	}
-
-// 	err = s.Deposit(account.ID, 10_000_00)
-// 	if err != nil {
-// 		t.Errorf("Reject(): can't deposit account, error = %v", err)
-// 		return
-// 	}
-
-// 	payment, err := s.Pay(account.ID, 1000_00, "auto")
-// 	if err != nil {
-// 		t.Errorf("Reject(): can't create payment, error = %v", err)
-// 		return
-// 	}
-
-// 	err = s.Reject(payment.ID)
-// 	if err != nil {
-// 		t.Errorf("Reject(): error = %v", err)
-// 		return
-// 	}
-// }
-
-// func TestService_FindPaymentByID_success(t *testing.T)  {
-// 	s := &Service{}
-
-// 	phone := types.Phone("+9920000001")
-// 	account, err := s.RegisterAccount(phone)
-// 	if err != nil {
-// 		t.Errorf("Reject(): can't register account, error = %v", err)
-// 		return
-// 	}
-
-// 	err = s.Deposit(account.ID, 10_000_00)
-// 	if err != nil {
-// 		t.Errorf("Reject(): can't deposit account, error = %v", err)
-// 		return
-// 	}
-
-// 	payment, err := s.Pay(account.ID, 1000_00, "auto")
-// 	if err != nil {
-// 		t.Errorf("Reject(): can't create payment, error = %v", err)
-// 		return
-// 	}
-
-// 	got, err := s.FindPaymentByID(payment.ID)
-// 	if err != nil {
-// 		t.Errorf("FindPaymentByID(): error = %v", err)
-// 		return
-// 	}
-
-// 	if !reflect.DeepEqual(payment, got) {
-// 		t.Errorf("FindPaymentByID(): wrong payment returned = %v", err)
-// 	}
-// }
-
-// func TestService_FindPaymentByID_fail(t *testing.T)  {
-// 	//создаём сервис
-// 	s := newTestService();
-
-// 	account, err := s.addAccountWithBalnce("+99200000001", 10_000_00)
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-
-// 	//осушествляем платёж на его счёт
-// 	payment, err := s.Pay(account.ID, 1000_00, "auto")
-// 	if err != nil {
-// 		t.Errorf("FindPaymentByID: can't create	payment, error = %v", err)
-// 		return
-// 	}
-// 	//пробуем найти несушествующий платёж
-// 	got, err := s.FindPaymentByID(uuid.New().String())
-// 	if err != nil {
-// 		t.Error("FindPaymentByID: must return error, returned nil")
-// 		return
-// 	}
-
-// 	if err!=ErrPaymentNotFound {
-// 		t.Errorf("FindPaymentByID(): must return ErrPaymnetNotFound, returned = %v", err)
-// 	}
-// }
-
 func TestService_FindPaymentByID_success(t *testing.T) {
 	//создаем сервис
 	s := newTestServiceUser()
@@ -264,5 +174,37 @@ func TestService_Repeat_success_user(t *testing.T) {
 	pay, err = s.Repeat(pay.ID)
 	if err != nil {
 		t.Errorf("Repeat(): can't payment for an account(%v), error(%v)",pay.ID, err)
+	}
+}
+
+
+func TestService_FavoritePayment_success_user(t *testing.T) {
+	//создаем сервис
+	var s Service
+
+	account, err := s.RegisterAccount("+9922000000")
+	if err != nil {
+		t.Errorf("method RegisterAccount return not nil error, account=>%v", account)
+		return
+	}
+	//пополняем баланс
+	err = s.Deposit(account.ID, 1000_00)
+	if err != nil {
+		t.Errorf("method Deposit return not nil error, error=>%v", err)
+	}
+	//pay
+	payment, err := s.Pay(account.ID, 100_00, "auto")
+	if err != nil {
+		t.Errorf("method Pay return not nil error, account=>%v", account)
+	}
+	//edit favorite
+	favorite, err := s.FavoritePayment(payment.ID, "auto")
+	if err != nil {
+		t.Errorf("method FavoritePayment returned not nil error, favorite=>%v", favorite)
+	}
+
+	paymentFavorite, err := s.PayFromFavorite(favorite.ID)
+	if err != nil {
+		t.Errorf("method PayFromFavorite returned nil, paymentFavorite(%v)", paymentFavorite)
 	}
 }
