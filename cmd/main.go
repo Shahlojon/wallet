c97a89f602801544d87dcf7ca17d2e348f7bdcb2
Shahlo <jaborova.shahlo@mail.ru>
2020-10-15 14:51:04 +0500

    update function

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,12 @@ import (
 	"os"
 	"log"
 	"github.com/Shahlojon/wallet/pkg/wallet"
-	//"fmt"
+	"fmt"
 )
 
 func main() {
 	//fmt.Println("hello")
-	svc :=&wallet.Service{}
+//	svc :=&wallet.Service{}
 	//svc.RegisterAccount("+992000000001")
 	//svc.Deposit(1, 10)
 	//svc.RegisterAccount("+992000000002")
@@ -40,46 +40,46 @@ func main() {
 	//svc.RegisterAccount("+992000000001")
 
 
-	// svc := &wallet.Service{}
-	// accountTest , err := svc.RegisterAccount("+992000000001")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// } 
+	svc := &wallet.Service{}
+	accountTest , err := svc.RegisterAccount("+992000000001")
+	if err != nil {
+		fmt.Println(err)
+		return
+	} 
 
-	// err = svc.Deposit(accountTest.ID, 100_000_00)
-	// if err != nil {
-	// 	switch err {
-	// 	case wallet.ErrAmountMustBePositive:
-	// 		fmt.Println("Сумма должна быть положительной")
-	// 	case wallet.ErrAccountNotFound:
-	// 		fmt.Println("Аккаунт пользователя не найден")		
-	// 	}		
-	// 	return
-	// }
-	// fmt.Println(accountTest.Balance)
+	err = svc.Deposit(accountTest.ID, 100_000_00)
+	if err != nil {
+		switch err {
+		case wallet.ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case wallet.ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователя не найден")		
+		}		
+		return
+	}
+	fmt.Println(accountTest.Balance)
 
-	// err = svc.Deposit(accountTest.ID, 200_000_00)
-	// if err != nil {
-	// 	switch err {
-	// 	case wallet.ErrAmountMustBePositive:
-	// 		fmt.Println("Сумма должна быть положительной")
-	// 	case wallet.ErrAccountNotFound:
-	// 		fmt.Println("Аккаунт пользователя не найден")		
-	// 	}		
-	// 	return
-	// }
-	// fmt.Println(accountTest.Balance)
+	err = svc.Deposit(accountTest.ID, 200_000_00)
+	if err != nil {
+		switch err {
+		case wallet.ErrAmountMustBePositive:
+			fmt.Println("Сумма должна быть положительной")
+		case wallet.ErrAccountNotFound:
+			fmt.Println("Аккаунт пользователя не найден")		
+		}		
+		return
+	}
+	fmt.Println(accountTest.Balance)
 
 
-	// newPay, err := svc.Pay(accountTest.ID,10_000_00,"auto")
-	// fmt.Println(accountTest.Balance)
-	// fmt.Println(newPay)
-	// fmt.Println(err)
+	newPay, err := svc.Pay(accountTest.ID,10_000_00,"auto")
+	fmt.Println(accountTest.Balance)
+	fmt.Println(newPay)
+	fmt.Println(err)
 
-	// fav, errFav := svc.FavoritePayment(newPay.ID, "Babilon")
-	// fmt.Println(errFav)
-	// fmt.Println(fav)
+	fav, errFav := svc.FavoritePayment(newPay.ID, "Babilon")
+	fmt.Println(errFav)
+	fmt.Println(fav)
    
 	wd, err := os.Getwd()
 	if err != nil {
@@ -87,7 +87,7 @@ func main() {
 		return
 	}
 
-	err = svc.Import(wd)
+	err = svc.Export(wd)
 	if err != nil {
 	 	log.Print(err)
 	 	return
